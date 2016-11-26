78f322e666d818d5c7e15c03e4fcea934a82ed40
Pavel Štross <pavel.stross@gmail.com>
2016-11-26 18:38:20 +0100

    `BallotPaper`s are embedded in `BallotBox`

--- app/models/ballot_box.rb
+++ app/models/ballot_box.rb
@@ -1,4 +1,5 @@
 class BallotBox
   include Mongoid::Document
   belongs_to :election
+  embeds_many :ballot_paper
 end
--- app/models/ballot_paper.rb
+++ app/models/ballot_paper.rb
@@ -1,4 +1,5 @@
 class BallotPaper
   include Mongoid::Document
   field :vote, type: String
+  embedded_in :ballot_box
 end
